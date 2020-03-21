9ae6201e160f96c997855fa27e0441e0cc876db9
Alexander Paul <exactfalse@gmail.com>
2020-03-22 01:23:49 +0800

    add header component, crown asset and links to home, shop and contact pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/home/home.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path='/' component={HomePage} />
 				<Route exact path='/shop' component={ShopPage} />
