4763846f62e856db424bff7810ea734b848a1fc2
Alexander Paul <exactfalse@gmail.com>
2020-03-26 05:08:03 +0800

    Add gh-pages and use HashRouter for the application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import './index.css'
@@ -10,9 +10,9 @@ import store from './redux/store'
 
 ReactDOM.render(
 	<Provider store={store}>
-		<BrowserRouter>
+		<HashRouter basename='/'>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</Provider>,
 	document.getElementById('root')
 )
