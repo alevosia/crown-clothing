f83d48367cdab1c34f0ce51fd6ede5c78dfe7fd0
Alexander Paul <exactfalse@gmail.com>
2020-04-06 23:45:02 +0800

    Disable no-console rule on error after saving user to Firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -43,7 +43,9 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 				...additionalData,
 			})
 		} catch (error) {
+			/* eslint-disable no-alert, no-console */
 			console.error('Error creating user', error)
+			/* eslint-enable no-alert, no-console */
 		}
 	}
 
