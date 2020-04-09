5245f7da31758455a85d00066acd4ee91453ccc0
Alexander Paul <exactfalse@gmail.com>
2020-04-09 16:39:21 +0800

    Move firebase config object to its own file

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+const config = {
+	apiKey: 'AIzaSyDl_PAGOMopzgg6uVmPkiCEXfMy9WzpSzU',
+	authDomain: 'crown-clothing-205db.firebaseapp.com',
+	databaseURL: 'https://crown-clothing-205db.firebaseio.com',
+	projectId: 'crown-clothing-205db',
+	storageBucket: 'crown-clothing-205db.appspot.com',
+	messagingSenderId: '144266235975',
+	appId: '1:144266235975:web:d26fcc535ed05a67976162',
+	measurementId: 'G-XWZCHF1HX8',
+}
+
+export default config
