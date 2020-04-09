26e7fd18df6dc90c7d74ebaef37a3d2b6962cbd9
Alexander Paul <exactfalse@gmail.com>
2020-04-09 16:02:50 +0800

    Add overflow-x: hidden to Body component

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -6,6 +6,7 @@ const BodyContainer = styled.div`
 	height: auto;
 	margin-left: auto;
 	margin-right: auto;
+	overflow-x: hidden;
 `
 
 const Body = (props) => <BodyContainer>{props.children}</BodyContainer>
